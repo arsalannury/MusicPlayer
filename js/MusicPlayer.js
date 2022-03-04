4b752d6bbc538f94501adb5b8ab6e1b8bd18fe3d
arsalannury <arsiiiland@yaho.com>
2022-03-04 22:23:48 +0330

    next btn handle

--- js/MusicPlayer.js
+++ js/MusicPlayer.js
@@ -1,12 +1,11 @@
-const musicCover = document.querySelector('.music_img');
-const musicName = document.querySelector('.music_name');
-const musicTime = document.querySelector('.music_time_number');
-const musicTimeRange = document.querySelector('.music_time');
-const playBtn = document.querySelector('#play_btn');
-const pauseBtn = document.querySelector('#pause_btn');
-const nextBtn = document.querySelector('#next_btn');
-const prevBtn = document.querySelector('#prev_btn');
-let currentMusic = 0;
+const musicCover = document.querySelector(".music_img");
+const musicName = document.querySelector(".music_name");
+const musicTime = document.querySelector(".music_time_number");
+const musicTimeRange = document.querySelector(".music_time");
+const playBtn = document.querySelector("#play_btn");
+const pauseBtn = document.querySelector("#pause_btn");
+const nextBtn = document.querySelector("#next_btn");
+const prevBtn = document.querySelector("#prev_btn");
 
 const musicsList = [
   {
@@ -41,12 +40,48 @@ const musicsList = [
   },
 ];
 
-let audio = musicsList[currentMusic].music;
+let currentMusic = 0;
 
-audio.addEventListener('timeupdate',(e) => {
-   musicTimeRange.value =  e.target.currentTime
-   musicTimeRange.max = e.target.duration
-})
-playBtn.addEventListener('click',(e) => {
-    audio.play()
-})
+let audio = musicsList[currentMusic].music;
+musicCover.src = musicsList[currentMusic].cover;
+musicName.innerText = musicsList[currentMusic].name;
+
+const changeMusic = () => {
+  audio = musicsList[currentMusic].music;
+  musicCover.src = musicsList[currentMusic].cover;
+  musicName.innerText = musicsList[currentMusic].name;
+};
+
+// audio.addEventListener("canplay", (e) => {
+//   musicTimeRange.max = e.target.duration;
+//   musicTimeRange.value = e.target.currentTime;
+//   console.log(22)
+// });
+
+audio.addEventListener("timeupdate", (e) => {
+  musicTimeRange.value = e.target.currentTime;
+  musicTimeRange.max = e.target.duration;
+  musicTime.innerText = Math.floor(e.target.currentTime);
+});
+
+musicTimeRange.addEventListener("input", (e) => {
+  audio.currentTime = e.target.value;
+});
+
+playBtn.addEventListener("click", (e) => {
+  if (audio.paused) {
+    audio.play();
+    e.target.classList.replace("bi-play-fill", "bi-pause-fill");
+  } else {
+    audio.pause();
+    e.target.classList.replace("bi-pause-fill", "bi-play-fill");
+  }
+});
+
+nextBtn.addEventListener("click", (e) => {
+  if(audio.played) audio.pause();
+  playBtn.classList.replace("bi-pause-fill", "bi-play-fill");
+  currentMusic += 1;
+  changeMusic();
+  musicTimeRange.value = audio.currentTime;
+});
